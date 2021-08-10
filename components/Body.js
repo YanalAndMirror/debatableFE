132755e8e0e34a826b030dbd1cd66f4ffedf32e6
yanalshmilan <yshmilan@gmail.com>
2021-08-10 07:49:30 +0300

    added notification and pagination

--- components/Body.js
+++ components/Body.js
@@ -12,6 +12,14 @@ export default function Body() {
         </div>
         <CardList />
       </div>
+      <div class="btn-group justify-center">
+        <button class="btn">Previous</button>
+        <button class="btn">1</button>
+        <button class="btn btn-active">2</button>
+        <button class="btn">3</button>
+        <button class="btn">4</button>
+        <button class="btn">Next</button>
+      </div>
     </>
   );
 }
--- components/CardList.js
+++ components/CardList.js
@@ -3,11 +3,13 @@ import CardItem from './CardItem';
 
 export default function CardList() {
   return (
-    <div class="grid md:grid-cols-3 gap-4 mt-4">
-      <CardItem title="Does God Exist" />
-      <CardItem title="Does God Exist" />
-      <CardItem title="Does God Exist" />
-      <CardItem title="Does God Exist" />
-    </div>
+    <>
+      <div class="grid md:grid-cols-3 gap-4 mt-4">
+        <CardItem title="Does God Exist" />
+        <CardItem title="Does God Exist" />
+        <CardItem title="Does God Exist" />
+        <CardItem title="Does God Exist" />
+      </div>
+    </>
   );
 }
