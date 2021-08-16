bafb91549f96c954631e5bcb387c3b113e48ff6f
mirrorcall96 <mirrorcall96@gmail.com>
2021-08-16 05:53:56 +0300

    Debate page + vote

--- components/Body/CardList.js
+++ components/Body/CardList.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardItem from "./CardItem";
 
 export default function CardList({ debates }) {
-  debates = debates.map((debate) => <CardItem title={debate.title} />);
+  debates = debates.map((debate) => <CardItem debate={debate} />);
   return (
     <>
       <div className="grid md:grid-cols-3 gap-4 mt-4 ml-32 mr-32">
